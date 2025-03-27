2b3964978cae8458c86ed17c372b9bcfcab266a1
maudhelen <mhovland.ieu2021@student.ie.edu>
2025-03-27 19:24:42 +0100

    correct labels

--- my-va-app/src/utils/emotions.js
+++ my-va-app/src/utils/emotions.js
@@ -2,7 +2,7 @@ export const emotions = [
   // Positive valence, high arousal (upper right - GREEN)
   { name: 'Happy', valence: 0.95, arousal: 0.5 },
   { name: 'Excited', valence: 0.75, arousal: 0.85 },
-  { name: 'Confident', valence: 0.7, arousal: 0.2 },
+  { name: 'Confident', valence: 0.8, arousal: 0.2 },
   { name: 'Aroused', valence: 0.2, arousal: 1.1 },
 
   // Positive valence, low arousal (lower right - YELLOW)
@@ -22,5 +22,7 @@ export const emotions = [
   { name: 'Depressed', valence: -1.1, arousal: -0.2 },
   { name: 'Sad', valence: -1.0, arousal: -0.55 },
   { name: 'Bored', valence: -0.5, arousal: -0.7 },
-  { name: 'Tired', valence: -0.2, arousal: -1.1 }
+  { name: 'Tired', valence: -0.2, arousal: -1.1 },
+
+  { name: 'Neutral', valence: 0.0, arousal: 0.0 }
 ]; 
